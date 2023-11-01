d58807c3e162f9f50c81e6ff22a9a5391d8e152c
layniebae <anaberkey113@gmail.com>
2023-10-31 22:02:29 -0500

    updates wire

--- EMAR391 Group Project/Assets/Scripts/Wire.cs
+++ EMAR391 Group Project/Assets/Scripts/Wire.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,56 @@ using UnityEngine;
 public class Wire : MonoBehaviour
 {
     public SpriteRenderer wireEnd;
+    public GameObject lightOn;
     Vector3 startPoint;
+    Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         startPoint = transform.parent.position;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
-   private void OnMouseDrag()
+    private void OnMouseDrag()
     {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
 
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+        foreach (Collider2D collider in colliders)
+        {
+            if(collider.gameObject != gameObject)
+            {
+                UpdateWire(collider.transform.position);
+
+                if (transform.parent.name.Equals(collider.transform.parent.name))
+                {
+                    collider.GetComponent<Wire>()?.Done();
+                    Done();
+                }
+                return;
+            }
+        }
+
+        UpdateWire(newPosition);
+    }
+
+    void Done()
+    {
+        lightOn.SetActive(true);
+
+        Destroy(this);
+    }
+
+    private void OnMouseUp()
+    {
+        UpdateWire(startPosition);
+    }
+
+    void UpdateWire(Vector3 newPosition)
+    {
         transform.position = newPosition;
 
         Vector3 direction = newPosition - startPoint;
@@ -25,6 +63,5 @@ public class Wire : MonoBehaviour
 
         float dist = Vector2.Distance(startPoint, newPosition);
         wireEnd.size = new Vector2(dist, wireEnd.size.y);
-
     }
 }
