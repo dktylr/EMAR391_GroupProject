769217865409faa31234aebe5fdf64c136e4912b
layniebae <anaberkey113@gmail.com>
2023-11-04 20:08:13 -0500

    puzzle finish

---
+++ EMAR391 Group Project/Assets/Laynie/Scripts (Laynie)/Rotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        if (!GameControl.youWin)
+            transform.Rotate(0f, 0f, 90f);
+    }
+
+}
