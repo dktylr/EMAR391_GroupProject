3060ad3168da4bfaefb3c47f8092e3eb83508b77
layniebae <anaberkey113@gmail.com>
2023-11-11 12:14:10 -0600

    puzzle sound

--- EMAR391 Group Project/Assets/Laynie/Scripts (Laynie)/Rotate.cs
+++ EMAR391 Group Project/Assets/Laynie/Scripts (Laynie)/Rotate.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+
+    public AudioSource spin;
+    private Rect audiorect;
+
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        audiorect = new Rect(20, 20, 100, 20);
+    }
     private void OnMouseDown()
     {
         if (!GameControl.youWin)
             transform.Rotate(0f, 0f, 90f);
-    }
 
-}
+            if (Input.GetMouseButtonDown(0))
+            {
+                spin.Play();
+            }
+        }
+    }
